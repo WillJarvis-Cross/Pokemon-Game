3f5ece324b32b0bbf278e794a48c2ad027ff7cd7
WillJarvis-Cross <72054183+WillJarvis-Cross@users.noreply.github.com>
2021-10-28 19:10:37 -0400

    Adding all necessary files

---
+++ Trainer.java
@@ -0,0 +1,19 @@
+public class /**
+ * @(#)Trainer.java
+ *
+ *
+ * @author
+ * @version 1.00 2018/5/21
+ */
+
+
+public class Trainer {
+    String speech;
+    Pokemon[] mons;
+    int tilex,tiley;
+    public Trainer() {
+    }
+
+
+}Trainer {
+}
